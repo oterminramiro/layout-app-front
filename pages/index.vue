c974ecf4b91b518cf6135fcc179e2ba5b3ae030e
Ramiro Otermin <oterminramiro@gmail.com>
2020-10-07 13:26:59 -0300

    up

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="row px-0 w-100">
+    	<div class="col-8 mx-auto px-0">
+			<h2>Bienvenido</h2>
+			<b-form @submit="onSubmit" @reset="onReset">
+				<b-form-group
+				id="input-group-1"
+				>
+					<b-form-input
+					id="input-1"
+					type="phone"
+					required
+					placeholder="Your phone"
+					size="lg"
+					></b-form-input>
+				</b-form-group>
+
+				<b-form-group id="input-group-4">
+					<b-form-checkbox size="lg">Acepto los <a href="#">terminos y condiciones</a></b-form-checkbox>
+				</b-form-group>
+
+				<b-button type="submit" block variant="outline-dark">Submit</b-button>
+			</b-form>
+    	</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+.container {
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
---
+++ pages/layout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row px-0 w-100">
+    	<div class="col-8 mx-auto px-0">
+			<h2>Layout</h2>
+			<div class="row" v-for="n in 6">
+				<div class="col" v-for="n in 5" style="height:50px;border:1px solid red">
+					{{ n }}
+				</div>
+			</div>
+
+    	</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+.container {
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+
+.title {
+	font-family:
+	'Quicksand',
+	'Source Sans Pro',
+	-apple-system,
+	BlinkMacSystemFont,
+	'Segoe UI',
+	Roboto,
+	'Helvetica Neue',
+	Arial,
+	sans-serif;
+	display: block;
+	font-weight: 300;
+	font-size: 100px;
+	color: #35495e;
+	letter-spacing: 1px;
+}
+
+</style>
