c821f1d8f3d65996d4c03e7a74c269ead429d0ea
ramiro <oterminramiro@gmail.com>
2020-10-13 16:23:27 -0300

    add login with redirects

--- pages/code.vue
+++ pages/code.vue
@@ -3,18 +3,11 @@
 		<div class="row">
 			<div class="col-6 mx-auto px-0 my-5">
 				<div class="text-center mx-auto">
-					<img class="img-fluid w-75" src="~/assets/undraw_book.svg" />
+					<img class="img-fluid w-75" src="~/assets/undraw_new_message.svg" />
 					<h2 class="my-5">Bienvenido</h2>
 				</div>
 				<b-form @submit="onSubmit" class="w-75 mx-auto">
-					<b-form-group id="input-group-1">
-						<VuePhoneNumberInput v-model="phone" required/>
-					</b-form-group>
-
-					<b-form-group id="input-group-4" class="text-center">
-						<b-form-checkbox size="sm" required>Acepto los <a href="#">terminos y condiciones</a></b-form-checkbox>
-					</b-form-group>
-
+					<b-form-input v-model="code" placeholder="Enter your code" required></b-form-input>
 					<b-button type="submit" class="mt-5" block variant="outline-dark">Submit</b-button>
 				</b-form>
 			</div>
@@ -24,13 +17,11 @@
 
 <script>
 	import axios from 'axios';
-	import VuePhoneNumberInput from 'vue-phone-number-input';
-	import 'vue-phone-number-input/dist/vue-phone-number-input.css';
 
 	export default {
 		data(){
 			return {
-				phone: '',
+				code: ''
 			}
 		},
 		methods: {
@@ -38,23 +29,23 @@
 
 				var config = {
 					method: 'post',
-					url: 'http://localhost:8000/api/customers/create',
+					url: 'http://localhost:8000/api/customers/login',
 					headers: {
 						'Content-Type': 'application/json'
 					},
-					data : JSON.stringify({"phone":this.phone.replace(/-|\s/g, '')})
+					data : JSON.stringify({
+						"code":this.code,
+						"id": localStorage.getItem("customer_id")
+					})
 				};
 
 				event.preventDefault();
 				axios(config)
 				.then(response => {
+					console.log(response.data)
 					if(response.data.success == 'true')
 					{
-						console.log(response.data)
-						if(process.browser){
-							localStorage.setItem("customer_id", response.data.id);
-						}
-						this.$router.push('/code')
+						this.$router.push('/main')
 					}
 				})
 				.catch(error => {
