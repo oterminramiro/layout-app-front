9bf6cdc7007c5cad9abedfb034a1cc41d0c22a0d
ramiro <oterminramiro@gmail.com>
2020-11-03 14:17:11 -0300

    update ports

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
 	server: {
-		port: 8000, // default: 3000
+		port: 3000, // default: 3000
 		host: '0.0.0.0' // default: localhost
 	},
 	// Target (https://go.nuxtjs.dev/config-target)
