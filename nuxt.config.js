07eb2c8c11103d7f08baa823f6de9f3d036edb48
ramiro <oterminramiro@gmail.com>
2020-11-04 12:43:26 -0300

    change config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,58 @@
+require('dotenv').config()
+
 export default {
-	server: {
-		port: 3000, // default: 3000
-		host: '0.0.0.0' // default: localhost
-	},
-	// Target (https://go.nuxtjs.dev/config-target)
-	target: 'static',
-	mode:'spa',
-	// Global page headers (https://go.nuxtjs.dev/config-head)
-	head: {
-		title: 'vue-app',
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' }
-		],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-		]
-	},
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'client',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
 	transition: 'page',
 
-	// Global CSS (https://go.nuxtjs.dev/config-css)
+  // Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [
 		'~/assets/main.css'
 	],
 
-	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-	plugins: [
-		'@/plugins/phone-number-input.js',
-		'@/plugins/vue-notification.js',
-		{ src: '~/plugins/vue-datepicker', mode: 'client' },
-		{ src: '~/plugins/vue-timepicker', mode: 'client' },
-	],
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+	  '@/plugins/phone-number-input.js',
+	  { src: '~/plugins/vue-notification', mode: 'client' },
+	  { src: '~/plugins/vue-datepicker', mode: 'client' },
+	  { src: '~/plugins/vue-timepicker', mode: 'client' },
+  ],
 
-	// Auto import components (https://go.nuxtjs.dev/config-components)
-	components: true,
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
 
-	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-	buildModules: [
-	],
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
 
-	modules: [
-		'bootstrap-vue/nuxt',
-		'@nuxtjs/axios',
-		'@nuxt/content',
-	],
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+	  'bootstrap-vue/nuxt',
+	  '@nuxtjs/axios',
+	  '@nuxt/content',
+  ],
 
-	axios: {
-		baseURL: process.env.API_URL,
-	},
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
 
-	// Content module configuration (https://go.nuxtjs.dev/config-content)
-	content: {},
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
 
-	// Build Configuration (https://go.nuxtjs.dev/config-build)
-	build: {
-
-	},
-
-	router: {
-		middleware: []
-	}
+  router: {
+	  middleware: []
+  }
 }
